2ed031c26161d6ff093b88db0642ab0bf97c788b
carloscmp <99369400+carloscmp@users.noreply.github.com>
2024-07-29 11:43:17 -0300

    Adição de botões e criação de funções para os mesmos

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog as fd
+from PIL import ImageTk, Image
 
 # cores
 co0 = "#2e2d2b"  # Preta
 co1 = "#feffff"  # Branca
 co2 = "#e5e5e5"  # grey
 co3 = "#00a095"  # Verde
-co4 = "#403d3d"   # letra
-co6 = "#003452"   # azul
-co7 = "#ef5350"   # vermelha
+co4 = "#403d3d"  # letra
+co6 = "#003452"  # azul
+co7 = "#ef5350"  # vermelha
 
-co6 = "#038cfc"   # azul
-co8 = "#263238"   # + verde
-co9 = "#e9edf5"   # + verde
+co6 = "#038cfc"  # azul
+co8 = "#263238"  # + verde
+co9 = "#e9edf5"  # + verde
 
-#Criando Janela
+# Criando Janela
 janela = Tk()
 janela.title('')
 janela.geometry('850x620')
@@ -27,7 +28,7 @@ janela.resizable(width=False, height=False)
 style = Style(janela)
 style.theme_use('clam')
 
-#Criando frames
+# Criando frames
 frame_logo = Frame(janela, width=850, height=52, bg=co6)
 frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
 
@@ -44,5 +45,87 @@ frame_detalhes.grid(row=4, column=0, pady=0, padx=10, sticky=NSEW)
 frame_tabela = Frame(janela, width=850, height=200, bg=co1)
 frame_tabela.grid(row=5, column=0, pady=0, padx=10, sticky=NSEW)
 
+# trabalhando no frame logo
+app_lg = Image.open('logo.png')
+app_lg = app_lg.resize((50, 50))
+app_lg = ImageTk.PhotoImage(app_lg)
+app_logo = Label(frame_logo, image=app_lg, text="Cadastro de Alunos", width=850, compound=LEFT, relief=RAISED,
+                 anchor=NW, font=('Ivy 15 bold'), bg=co6, fg=co1)
+app_logo.place(x=0, y=0)
+
+
+# função para cadastrar alunos
+def alunos():
+    print('Aluno')
+
+
+# função para adicionar cursos e turmas
+def adicionar():
+    print('Cursos e turmas')
+
+
+# função para salvar
+def salvar():
+    print('Salvar')
+
+
+# função de controle
+def control(i):
+    # cadastro de aluno
+    if i == 'cadastro':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função alunos
+        alunos()
+
+    if i == 'adicionar':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função adicionar
+        adicionar()
+
+    if i == 'salvar':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função salvar
+        salvar()
+
+
+# criando botoes
+app_img_cadastro = Image.open('add.png')
+app_img_cadastro = app_img_cadastro.resize((18, 18))
+app_img_cadastro = ImageTk.PhotoImage(app_img_cadastro)
+app_cadastro = Button(frame_dados, command=lambda: control('cadastro'), image=app_img_cadastro, text="Cadastro",
+                      width=100, compound=LEFT, overrelief=RIDGE,
+                      font=('Ivy 11'), bg=co1, fg=co0)
+app_cadastro.place(x=10, y=30)
+
+app_img_adicionar = Image.open('add.png')
+app_img_adicionar = app_img_adicionar.resize((18, 18))
+app_img_adicionar = ImageTk.PhotoImage(app_img_adicionar)
+app_adicionar = Button(frame_dados, command=lambda: control('adicionar'), image=app_img_adicionar, text="Adicionar",
+                       width=100, compound=LEFT, overrelief=RIDGE,
+                       font=('Ivy 11'), bg=co1, fg=co0)
+app_adicionar.place(x=123, y=30)
+
+app_img_salvar = Image.open('save.png')
+app_img_salvar = app_img_salvar.resize((18, 18))
+app_img_salvar = ImageTk.PhotoImage(app_img_salvar)
+app_salvar = Button(frame_dados, command=lambda: control('salvar'), image=app_img_salvar, text="Salvar",
+                    width=100, compound=LEFT, overrelief=RIDGE,
+                    font=('Ivy 11'), bg=co1, fg=co0)
+app_salvar.place(x=236, y=30)
 
 janela.mainloop()
